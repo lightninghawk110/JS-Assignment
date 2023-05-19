09b6864952336bfcb8661ef7da2e01b3e4919d3a
Setu Raj <92735660+lightninghawk110@users.noreply.github.com>
2023-05-19 20:44:19 +0530

    Assignment 1
    Solution for Create a function that takes two numbers as parameters and returns their sum.

---
+++ SumFunc.js
@@ -0,0 +1,8 @@
+//Assignment - Create a function that takes two numbers as parameters and returns their sum.
+
+function add(num1, num2) {
+    return num1 + num2;
+  }
+
+  let result = add(5, 10);
+  console.log(result);
