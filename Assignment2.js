08d333ed315a256332be5ad698cab6d8d8f4b17d
Setu Raj <92735660+lightninghawk110@users.noreply.github.com>
2023-05-20 21:14:19 +0530

    Assignment 2
    1.Create an array called "fruits" with the following fruits: "apple", "banana", "orange", "grape", "mango".
    2.Use JavaScript to display the total number of fruits in the array

---
+++ Assignment2.js
@@ -0,0 +1,11 @@
+
+//Create an array called "fruits" with the following fruits: "apple", "banana", "orange", "grape", "mango".
+
+var fruits = ["apple", "banana", "orange", "grape", "mango"];
+for (var i = 0; i < fruits.length; i++) {
+    console.log(fruits[i]);
+  }
+
+  //Use JavaScript to display the total number of fruits in the array
+  
+  console.log("Total Fruits are:" + fruits.length);
